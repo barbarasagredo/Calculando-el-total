7e8ca8faa5bdd31ae8972f9f40189b89aafdff13
Bárbara Sagredo <barpaz.sc@gmail.com>
2025-08-24 15:50:14 -0400

    se modifican los nombres de las variables y se agrega la lógica en js

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-precio = 400000
+price = 400000;
+priceSpan = document.querySelector(".initial-price");
+priceSpan.innerHTML = price;
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+amount = 0;
+amountSpan = document.querySelector(".amount");
+amountSpan.innerHTML = amount;
+
+totalToPay = 0;
+totalSpan = document.querySelector(".total");
+totalSpan.innerHTML = totalToPay;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const subtractButton = document.getElementById("subtractButton");
+  const addButton = document.getElementById("addButton");
+
+  addButton.onclick = () => {
+    let number = Number(amountSpan.innerHTML);
+    let addAmount = number + 1;
+
+    amountSpan.innerHTML = addAmount;
+    totalToPay = addAmount * price;
+    totalSpan.innerHTML = totalToPay;
+  };
+
+  subtractButton.onclick = () => {
+    let number = Number(amountSpan.innerHTML);
+    let subtractAmount = number - 1;
+
+    amountSpan.innerHTML = subtractAmount;
+    totalToPay = subtractAmount * price;
+    totalSpan.innerHTML = totalToPay;
+  };
+});
